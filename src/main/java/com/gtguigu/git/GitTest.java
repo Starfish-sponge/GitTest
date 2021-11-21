ac98734b2c090215770d24bf467ec2e7e0a207b1
mazhuang <1097922133@qq.com>
2021-11-21 15:44:03 +0800

    first commit

---
+++ src/main/java/com/gtguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.gtguigu.git;
+
+/**
+ * @title: GitTest
+ * @Author Mazhuang
+ * @Date: 2021/11/21 15:40
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
